3ebdc0e02585509237bf0381b168e6f82f6a7943
TonyReignsT <tonyreigns00@gmail.com>
2024-08-03 17:59:50 +0300

    React search filter

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import React , {useState} from 'react';
+import data from './ProductData.json';
+
+export default function App(){
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleChange = (event) => {setSearchTerm(event.target.value)}
+    return(
+        <>
+            <div className='container'>
+                <div className='searchInput'>
+                    <input 
+                    className='searchInput' 
+                    type='text' 
+                    placeholder='Search here...' 
+                    onChange={handleChange}
+                    />
+                </div>
+                <div className='card_Container'>
+                    {
+                        data.filter((val)=>{
+                            if (searchTerm === ""){
+                                return val;
+                            } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                                return val;
+                            }
+                        }).map((val)=>{
+                            return(
+                                <div className='card' key={val.id}>
+                                    <img src={val.image} alt='' />
+                                    <p>{val.title}</p>
+                                    <p className='price'>${val.price}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
